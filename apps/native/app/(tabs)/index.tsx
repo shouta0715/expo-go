b637b17ccec744abd8cff52cf167846e96ee95ab
shouta0715 <kshouta0715@gmail.com>
2025-08-15 19:53:07 +0900

    tab layout

--- apps/native/app/(tabs)/index.tsx
+++ apps/native/app/(tabs)/index.tsx
@@ -1,13 +1,9 @@
-import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Hello World</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About Page
-      </Link>
     </View>
   );
 }
--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -1,20 +1,17 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { LogBox } from "react-native";
+
+LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          headerTitle: "Sticker Smash",
-          headerLeft: () => <></>,
-        }}
-      />
-      <Stack.Screen
-        name="about"
-        options={{ headerTitle: "About", headerBackTitle: "Back" }}
-      />
-      <Stack.Screen name="+not-found" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <StatusBar style="light" />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 }
