450925b5ecffed17d9d710d6f3ffd10a3941a37b
shouta0715 <kshouta0715@gmail.com>
2025-08-16 22:07:35 +0900

    chore

--- apps/router/src/app/index.tsx
+++ apps/router/src/app/index.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { AppText } from "@/components/app-text";
 
 export default function Index() {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View className="flex-1 justify-center p-4">
+      <AppText center>
+        Open up <AppText bold>app/index.tsx</AppText> to start working on your
+        app!
+      </AppText>
     </View>
   );
 }
